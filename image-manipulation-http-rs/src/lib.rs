96693bbe0a824832d01ac915d047beca2e03c442
Radu Matei <radu@fermyon.com>
2024-11-08 12:25:37 +0100

    Update image manipulation service
    Signed-off-by: Radu Matei <radu@fermyon.com>

--- image-manipulation-http-rs/src/lib.rs
+++ image-manipulation-http-rs/src/lib.rs
@@ -8,6 +8,7 @@ use bindings::deps::component::image_manipulation_lib::image_manipulation;
 /// A simple Spin HTTP component.
 #[http_component]
 fn handle_image_manipulation_http_rs(req: Request) -> anyhow::Result<impl IntoResponse> {
+    println!("Calling image manipulation component dependency from Rust");
     let img = image_manipulation::grayscale(&req.body().to_vec(), 100)?;
     Ok(Response::builder()
         .status(200)
